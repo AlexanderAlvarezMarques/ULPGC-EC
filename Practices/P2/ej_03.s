17d243088caeac89c050ab5a2c15bff8da2333b2
Alexander Álvarez Marques <31989083+alex222en@users.noreply.github.com>
2018-10-03 14:29:13 +0100

    Update ej_03.s

--- Practices/P2/ej_03.s
+++ Practices/P2/ej_03.s
@@ -1,4 +1,22 @@
-	.global _start
+despIzq:
+
+	# Cogemos y desplazamos a la izq el registro de led's verdes
+	ldwio r5, 0(r4)
+	slli r5, r5, 8
+
+	# Se paso del limite de 8 bits
+	beq r5, r0, reiniciar
+	stwio r5, 0(r4)
+	ret
+
+reiniciar:
+
+	movia r5, vDisplay
+	ldb r5, 0(r5)
+	stwio r5, 0(r4)
+	ret
+
+.global _start
 
 _start:
 
@@ -7,31 +25,31 @@ _start:
 
 	# Cargamos el numero correspondiente
 	movia r5, vDisplay
-	ldw r5, 0(r5)
+	ldbu r5, 0(r5)
 
 display:
 
 	# Encendemos display
-	stw r5, 0(r4)
+	stwio r5, 0(r4)
 
 	# Desplazamos r5
-	slri r5, 8
+	call despIzq
 
 contador:
 
-	# Cargamos el valor del contador
+	# Cargamos contador de programa
 	movia r23, timer
 	ldw r23, 0(r23)
 
-pBucle:
+cBucle:
 
 	beq r23, r0, display
-	addi r23, r23, -0x01
-	br pBucle
+	addi r23, r23, -1
+	br cBucle
 
 .data
 
-vDisplay:	.byte 0x3F,0x06,0x5B,0x4F,0x36,0x3D,0x7C,0x03,0x7F,0x6F # 0,1,2,3,4,5,6,7,8,9
-timer:		.word 250000
+vDisplay:	.byte 0x3F,0x06,0x5B,0x4F,0x36,0x3D,0x7C,0x03,0x7F,0x6F,0,0 # 0,1,2,3,4,5,6,7,8,9
+timer:		.word 500000
 
 .end
