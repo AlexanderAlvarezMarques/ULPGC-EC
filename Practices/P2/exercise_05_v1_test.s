62c6f11b1acce7bf04d8249b83d8c7f34228802b
Alexander Álvarez Marques <31989083+alex222en@users.noreply.github.com>
2018-10-24 14:23:03 +0100

    Actualización del ejercico

--- Practices/P2/exercise_05_v1_test.s
+++ Practices/P2/exercise_05_v1_test.s
@@ -8,83 +8,89 @@
 .global EXCEPTIONS_HANDLER
 
 EXCEPTIONS_HANDLER:
-    # Creacion de pila
-	movia sp, pila
     
-    # Guardamos et en pila
-    subi sp, sp, 12
-    stwio et, 4(sp)
-    
-    # Guardamos registros usados
-    stwio r7, 8(sp)
-    stwio r23, 12(sp)
-    
-    # Comprobamos si es una interrupcion interna
-    beq et, r0, fin
-    
-    # Como es interna comprobamos quien interrumpe
-    # Botones
-    andi r7, et, 2
-    beq r7, r0, fin
-    
-boton:
+    # Actualizamos pila
+    subi sp, sp, 28
 
-	# Comprobamos boton pulsado
-    ldwio r7, 0xC(r5)
-    
-    # Boton 1
-    movia r23, 1
-    and r7, r7, r23
-    beq r7, r23, boton_1
-    
-    # Comprobamos boton pulsado
-    ldwio r7, 0xC(r5)
-    
-    # Boton 1
-    movia r23, 2
-    and r7, r7, r23
-    beq r7, r23, boton_2
-    
+    # Guardamos registros en la pila
+    stw et, 4(sp)
+    stw r7, 8(sp)
+    stw r8, 12(sp)
+    stw r9, 16(sp)
+
+    # Comprobamos tipo de interrupcion
+    rdctl et, ctl4 # ctl4 I - Pending
+
+    # Si es 0 es interrupcion interna
+    beq r7, r0, fin
+
+    # Interrupcion externa, actualizo return adress
+    subi ea, ea, 4
+
+    # Comprobamos quien ha interrumpido
+    andi r7, et, 2
+    beq r7, r0, fin # Como no son botones nos vamos a fin
+
+botones:
+
+	# Leo el estado de los botones
+	ldwio r7, 0xC(r5)
+
+	# Reinicio interrupcion del botones
+	stwio r0, 0xC(r5)
+
+	# Comprobamos que boton interrumpe
+	movia r8, 2
+	and r9, r7, r8
+	beq r9, r8, boton_1
+
+	movia r8, 4
+	and r9, r7, r8
+	beq r9, r8, boton_2
+
+	movia r8, 8
+	and r9, r7, r8
+	beq r9, r8, boton3
+
+	br fin
+
 boton_3:
 
-	# Cargamos el valor 3 en registro r23 que esta actualmente en sp
-    ldwio r7, 3(r6)
-    stwio r7, 12(sp)
-    
-    # Saltamos a fin
-    br fin
+	ldb r8, 3(r6)
+	stw r8, 12(sp)
+	br fin
 
 boton_2:
 
-	# Cargamos el valor 3 en registro r23 que esta actualmente en sp
-    ldwio r7, 2(r6)
-    stwio r7, 12(sp)
-    
-    # Saltamos a fin
-    br fin
-    
+	ldb r8, 2(r6)
+	stw r8, 12(sp)
+	br fin
+
 boton_1:
 
-	# Cargamos el valor 3 en registro r23 que esta actualmente en sp
-    ldwio r7, 1(r6)
-    stwio r7, 12(sp)
+	ldb r8, 1(r6)
+	stw r8, 12(sp)
 
 fin:
 
-	# Recuperamos registros
-    ldwio et, 4(sp)
-    ldwio r7, 8(sp)
-    ldwio r23, 12(sp)
-    
-    # Retornamos pila
-    addi sp, sp, 12
-    
-    # Volvemos al programa
-    ret
+	# Restauro registros
+	ldw et, 4(sp)
+    ldw r7, 8(sp)
+    ldw r8, 12(sp)
+    stw r9, 16(sp)
+
+    # Restauro pila
+    addi sp, sp, 28
+
+    # Retorno al programa
+    eret
 
 .global _start
 _start:
 	
+	# Creamos pila
+	movia sp, 0x007FFFFC
+
 	# Carga de direcciones
     movia r4, 0x10000020 # Displays
     movia r5, 0x10000050 # PushButton
@@ -102,21 +108,14 @@ _start:
     movia r7, 0b001
     wrctl status, r7
     
-    # Usamos r23 como registro de escritura para display
-    ldbio r23, 0(r6)
+    # Usamos r8 como registro de escritura para display
+    ldbio r8, 0(r6)
     
 mostrarDisplay:
 
-	stwio r23, 0(r4)
+	stwio r8, 0(r4)
     br mostrarDisplay
     
 .data
 
 numeros:	.byte	0x3F,0x06,0x5B,0x4F,0x36,0x3D,0x7C,0x03,0x7F,0x6F # 0,1,2,3,4,5,6,7,8,9
-
-.org 0x00100000
-pila:	.skip 32
-
-
-
-
